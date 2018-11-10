a672921460fbfb2b2dc190991170e806812170ce
liangzhixin <zhixin.liang@jieshunpay.cn>
2018-11-10 16:54:56 +0800

    zuul过滤器

--- api-gateway/src/main/java/com/example/apigateway/filter/AccessFilter.java
+++ api-gateway/src/main/java/com/example/apigateway/filter/AccessFilter.java
@@ -4,6 +4,7 @@ import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,21 +13,42 @@ import javax.servlet.http.HttpServletRequest;
 @Slf4j
 public class AccessFilter extends ZuulFilter {
 
+    /**
+     * 该函数需要返回一个字符串来代表过滤器的类型,Zuul中默认定义了四种不同生命周期的过滤器类型:
+     *      pre: 在请求路由之前调用
+     *      routing: 在路由请求时候被调用
+     *      post: 在routing和error过滤器之后被调用
+     *      error: 处理请求时发生错误被调用
+     * @return String
+     */
     @Override
     public String filterType() {
-        return "pre";
+        return FilterConstants.PRE_TYPE;
     }
 
+    /**
+     * 通过int值来定义过滤器的执行顺序,数值越小优先级越高
+     * @return int
+     */
     @Override
     public int filterOrder() {
         return 0;
     }
 
+    /**
+     * 返回一个boolean类型来判断该过滤器是否要执行
+     * @return boolean
+     */
     @Override
     public boolean shouldFilter() {
         return true;
     }
 
+    /**
+     * 过滤器的具体逻辑
+     * @return Object
+     * @throws ZuulException
+     */
     @Override
     public Object run() throws ZuulException {
         RequestContext requestContext = RequestContext.getCurrentContext();
